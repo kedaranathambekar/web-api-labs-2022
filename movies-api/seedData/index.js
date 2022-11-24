898ed59c19b74302505d2b59310bd3a7353bc324
Kedaranath Ambekar <72230613+kedaranath3967@users.noreply.github.com>
2022-11-24 00:41:06 +0000

    Topic 10 - Update Movies Data/Route

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -3,6 +3,8 @@ import genreModel from '../api/genres/genresModel';
 import users from './users';
 import dotenv from 'dotenv';
 import genres from './genres';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 dotenv.config();
 
 // deletes all user documents in collection and inserts test data
@@ -27,8 +29,20 @@ async function loadGenres() {
       console.error(`failed to Load user Data: ${err}`);
     }
   }
+  export async function loadMovies() {
+    console.log('load seed data');
+    console.log(movies.length);
+    try {
+      await movieModel.deleteMany();
+      await movieModel.collection.insertMany(movies);
+      console.info(`${movies.length} Movies were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load movie Data: ${err}`);
+    }
+  }
 
 if (process.env.SEED_DB) {
   loadUsers();
   loadGenres();
+  loadMovies();//ADD THIS LINE
 }
