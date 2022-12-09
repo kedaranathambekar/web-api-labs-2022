2520ed2e22f947159252b5e2a99be835ff0392ca
Kedaranath Ambekar <72230613+kedaranath3967@users.noreply.github.com>
2022-12-09 11:57:20 +0000

    Topic 11 - i have completed the lab.

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { movies, movieReviews, movieDetails } from './moviesData';
+import {  movieReviews } from './moviesData';
 import uniqid from 'uniqid';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
-
+import {
+    getUpcomingMovies
+  } from '../tmdb-api';
 const router = express.Router(); 
 router.get('/', asyncHandler(async (req, res) => {
     let { page = 1, limit = 10 } = req.query; // destructure page and limit and set default values
@@ -61,4 +63,8 @@ router.post('/:id/reviews', (req, res) => {
         });
     }
 });
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
 export default router;
